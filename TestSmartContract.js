d4f21702ca1d5da7ed53dc32dfbc73b919368b77
nicklatkovich <nicklatkovich@gmail.com>
2017-12-09 04:27:07 +0300

    No more using infura

--- TestSmartContract.js
+++ TestSmartContract.js
@@ -1,84 +1,12 @@
 const Web3 = require('web3');
-let web3 = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io"));
+const Config = require('./contracts/testcontract-config.json')
 
-var CoursetroContract = web3.eth.contract([{
-    "constant": true,
-    "inputs": [],
-    "name": "getOwnerAddress",
-    "outputs": [{"name": "", "type": "address"}],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [],
-    "name": "getBalance",
-    "outputs": [{"name": "", "type": "uint256"}],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [{"name": "count", "type": "uint256"}],
-    "name": "buyTokens",
-    "outputs": [],
-    "payable": true,
-    "stateMutability": "payable",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [],
-    "name": "getTokenPrice",
-    "outputs": [{"name": "", "type": "uint256"}],
-    "payable": false,
-    "stateMutability": "pure",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [{"name": "count", "type": "uint256"}],
-    "name": "sellTokens",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [],
-    "name": "destroy",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [],
-    "name": "sellTokens",
-    "outputs": [{"name": "soldTokensCount", "type": "uint256"}],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [{"name": "adr", "type": "address"}],
-    "name": "getBalanceOf",
-    "outputs": [{"name": "", "type": "uint256"}],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [],
-    "name": "buyTokens",
-    "outputs": [{"name": "boughtTokensCount", "type": "uint256"}],
-    "payable": true,
-    "stateMutability": "payable",
-    "type": "function"
-}, {"inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor"}]);
-
-let contractUid = '0x3630adc02417ccb1c037cccd4fbc6c32ff4a1bd4';
+let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+var CoursetroContract = web3.eth.contract(Config.interface);
+let contractUid = Config.address;
 var Coursetro = CoursetroContract.at(contractUid);
 
-console.log(web3.eth.accounts);
+console.log('Blocks count: ' + web3.eth.blockNumber);
 
 //1
 let balance = web3.fromWei(Coursetro.getTokenPrice().toNumber(), 'ether');
