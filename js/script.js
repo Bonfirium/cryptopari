449c3bd5f86fe85dcc64594a7429dd88949af46f
djomaa <djoma.mosko@gmail.com>
2017-12-09 15:57:19 +0300

    fixed head-menu

--- js/script.js
+++ js/script.js
@@ -1,45 +1,43 @@
 "use strict";
 function createPage() {
-    create_headMenu(1);
+    create_headMenu();
 
 }
-function create_headMenu( activeIndex = -1) {
+function create_headMenu( activeIndex = -1 ) {
     var navItems = [
         {href: 'http://google.com', text: 'Google'},
         {href: 'http://bing.com', text: 'Bing'},
         {href: 'http://stackoverflow.com', text: 'StackOverflow'}
     ];
-    // A few variables for use later
     var navElem = document.createElement("nav"),
         navList = document.createElement("ul"), 
-        navItem, navLink;
+        navLogo = document.createElement("img"),
+        navItem = document.createElement("li"),
+        navLink;
+
     navElem.className = "head-menu";
+
+    navLogo.className = "brand";
+    navLogo.src = ".\\images\\headMenuLogo.png";
+    navItem.appendChild(navLogo);
+    navList.appendChild(navItem);
+
     navElem.appendChild(navList);
 
-    // Cycle over each nav item
     for (var i = 0; i < navItems.length; i++) {
-        // Create a fresh list item, and anchor
         navItem = document.createElement("li");
         navLink = document.createElement("a");
-
-        // Set properties on anchor
         navLink.href = navItems[i].href;
         navLink.innerHTML = navItems[i].text;
-        //navLink.className = "head-menu"
-
-        // Add anchor to list item, and list item to list
         navItem.appendChild(navLink);
         navList.appendChild(navItem);
     }
 
     if ( activeIndex > -1 ) {
-        navList.children[0].className = "active";
+        navList.children[ activeIndex + 1 ].className = "active";
         alert();
     }
-    // Set first list item as current
 
-
-    // Add list to body (or anywhere else)
     // window.onload = function () {
     document.body.appendChild(navElem);
     // }
