d010d74c88719726c87ea8e6ce819a4809f10e86
DELL <gor_nick987@mail.ru>
2017-12-09 08:09:43 +0300

    reformated code style

--- testing/test-simple-contract.js
+++ testing/test-simple-contract.js
@@ -1,16 +1,15 @@
+const Web3 = require('web3');
+const Config = require('../config.json');
+const ContractConfig = require('../contracts/testcontract-config.json');
 
-const Web3 = require('web3')
-const Config = require('../config.json')
-const ContractConfig = require('../contracts/testcontract-config.json')
-
-async function main( ) {
-    let web3 = new Web3(new Web3.providers.HttpProvider(Config.httpProvider))
-    console.log('Blocks count: ' + await web3.eth.getBlockNumber( ))
-    let contract = new web3.eth.Contract(ContractConfig.interface, ContractConfig.address)
-    let ownerAdress = await contract.methods.getOwnerAddress( ).call( )
-    console.log('Contract owner address is ' + ownerAdress)
-    console.log('Owner token balance is ' + await contract.methods.getBalanceOf(ownerAdress).call( ))
-    console.log('Token price is ' + web3.utils.fromWei(await contract.methods.getTokenPrice( ).call( )) + ' ETH')
+async function main() {
+    let web3 = new Web3(new Web3.providers.HttpProvider(Config.httpProvider));
+    console.log('Blocks count: ' + await web3.eth.getBlockNumber());
+    let contract = new web3.eth.Contract(ContractConfig.interface, ContractConfig.address);
+    let ownerAddress = await contract.methods.getOwnerAddress().call();
+    console.log('Contract owner address is ' + ownerAddress);
+    console.log('Owner token balance is ' + await contract.methods.getBalanceOf(ownerAddress).call());
+    console.log('Token price is ' + web3.utils.fromWei(await contract.methods.getTokenPrice().call()) + ' ETH');
 }
 
-main( )
+main();
