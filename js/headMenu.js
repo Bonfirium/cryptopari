06a26836eb37487741ab0c4a6f4245e4bf391038
sawdsf <p.lev4ik@yandex.ru>
2017-12-09 22:04:55 +0300

    MatchPage fix

--- js/headMenu.js
+++ js/headMenu.js
@@ -1,8 +1,7 @@
 function create_headMenu( activeIndex = -1 ) {
     var navItems = [
         {href: "./main.html", text: "Main Page"},
-        {href: "./matches.html", text: "Matches"},
-        {href: "./match.html", text: "Match"}
+        {href: "./matches.html", text: "Matches"}
     ];
     var navElem  = document.createElement("nav"),
         navList  = document.createElement("ul"), 
