0c08188c8bcce03ed2db40b7bc6720d42a42bf5e
djomaa <djoma.mosko@gmail.com>
2017-12-09 14:59:28 +0300

    head-menu

--- js/script.js
+++ js/script.js
@@ -1,55 +1,47 @@
-// function append() {
-//     var btndiv = document.createElement("div");
-//     btndiv.id = 'divBtn';
-//     for (i = 0; i < butene.length; i++)
-//     {
-//         btndiv.innerHtml = '<input type="button" value=butene[i]>';
-//         document.body.appendChild(btndiv);
-//     }
-// }
+"use strict";
 function createPage() {
-    headMenu.create();
+    create_headMenu(1);
+
 }
-class headMenu {
-    create() {
-        var navItems = [
-            {href: 'http://google.com', text: 'Google'},
-            {href: 'http://bing.com', text: 'Bing'},
-            {href: 'http://stackoverflow.com', text: 'StackOverflow'}
-        ];
-        // A few variables for use later
-        var navElem = document.createElement("nav"),
-            navList = document.createElement("ul"), 
-            navItem, navLink;
-        navElem.className = "head-menu";
-        navElem.appendChild(navList);
+function create_headMenu( activeIndex = -1) {
+    var navItems = [
+        {href: 'http://google.com', text: 'Google'},
+        {href: 'http://bing.com', text: 'Bing'},
+        {href: 'http://stackoverflow.com', text: 'StackOverflow'}
+    ];
+    // A few variables for use later
+    var navElem = document.createElement("nav"),
+        navList = document.createElement("ul"), 
+        navItem, navLink;
+    navElem.className = "head-menu";
+    navElem.appendChild(navList);
 
-        // Cycle over each nav item
-        for (var i = 0; i < navItems.length; i++) {
-            // Create a fresh list item, and anchor
-            navItem = document.createElement("li");
-            navLink = document.createElement("a");
+    // Cycle over each nav item
+    for (var i = 0; i < navItems.length; i++) {
+        // Create a fresh list item, and anchor
+        navItem = document.createElement("li");
+        navLink = document.createElement("a");
 
-            // Set properties on anchor
-            navLink.href = navItems[i].href;
-            navLink.innerHTML = navItems[i].text;
-            //navLink.className = "head-menu"
+        // Set properties on anchor
+        navLink.href = navItems[i].href;
+        navLink.innerHTML = navItems[i].text;
+        //navLink.className = "head-menu"
 
-            // Add anchor to list item, and list item to list
-            navItem.appendChild(navLink);
-            navList.appendChild(navItem);
-        }
-
-        // Set first list item as current
-
-
-        // Add list to body (or anywhere else)
-        // window.onload = function () {
-        //     document.body.appendChild(navElem);
-        // }
-    }
-    choose( name ) {
-        
+        // Add anchor to list item, and list item to list
+        navItem.appendChild(navLink);
+        navList.appendChild(navItem);
     }
 
+    if ( activeIndex > -1 ) {
+        navList.children[0].className = "active";
+        alert();
+    }
+    // Set first list item as current
+
+
+    // Add list to body (or anywhere else)
+    // window.onload = function () {
+    document.body.appendChild(navElem);
+    // }
+    
 }
