abd72f471eaaf522d84bd46f55e15fa266123405
DELL <gor_nick987@mail.ru>
2017-12-10 03:12:52 +0300

    upd smth hihihi

--- testing/test-simple-contract.js
+++ testing/test-simple-contract.js
@@ -4,12 +4,16 @@ const ContractConfig = require('../contracts/testcontract-config.json');
 
 async function main() {
     let web3 = new Web3(new Web3.providers.HttpProvider(Config.httpProvider));
+
     console.log('Blocks count: ' + await web3.eth.getBlockNumber());
     let contract = new web3.eth.Contract(ContractConfig.interface, ContractConfig.address);
     let ownerAddress = await contract.methods.getOwnerAddress().call();
     console.log('Contract owner address is ' + ownerAddress);
     console.log('Owner token balance is ' + await contract.methods.getBalanceOf(ownerAddress).call());
     console.log('Token price is ' + web3.utils.fromWei(await contract.methods.getTokenPrice().call()) + ' ETH');
+
+
+
 }
 
 main();
