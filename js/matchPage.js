b85211f9a8779b8a8800c6d748a1ae9c048a7a7c
djomaa <djoma.mosko@gmail.com>
2017-12-10 19:02:19 +0300

    last commit

--- js/matchPage.js
+++ js/matchPage.js
@@ -2,6 +2,6 @@ window.onload = function () {
     path = window.location.href;
     pars = (path.substr( path.indexOf("?")+1 )).split("/");
     console.log(pars);
-    alert("check console. :) ");
+    alert("indexex in the console");
     //create_headMenu(2);
 }
